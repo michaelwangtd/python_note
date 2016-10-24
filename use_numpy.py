2184030b704ea2e5878b010a5f573be6d402220b
CaptWang <michaelwangtd@gmail.com>
2016-10-24 20:14:33 +0800

    add use_numpy.py

---
+++ use_numpy.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+
+import numpy
+
+# classesLabels = [-1,-1,1,1,1]
+# labMat = numpy.mat(classesLabels).transpose()
+# print(labMat)
+# zerosMt = numpy.mat(numpy.zeros((5,1)))
+# print(zerosMt)
+#
+# print(numpy.multiply(zerosMt,labMat))
+
+
+# ## 测试 multiply()函数
+# testM1 = numpy.mat([1,2,3,4]).transpose()
+# print(testM1)
+# testM2 = numpy.mat([-1,2,-3,4]).transpose()
+# print(testM2)
+# print(numpy.multiply(testM1,testM2))
+
+
+
+# print(type(numpy.zeros((5,1))))     #  <class 'numpy.ndarray'>
+# print(type(numpy.mat(numpy.zeros((5,1)))))      #  <class 'numpy.matrixlib.defmatrix.matrix'>
